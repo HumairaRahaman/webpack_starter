f97cb983934e21f31a2c2aae49e63a8139d41de9
Humaira Rahaman <humairamitul@gmail.com>
2022-06-02 21:24:16 +0600

    create a webpack bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin') 
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
 module.exports = {
     mode: 'development',
@@ -12,7 +13,10 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name][contenthash].js',
         clean: true,
+        assetModuleFilename: "[name][ext]"
+        
     },
+    devtool: 'source-map',
     devServer: {
         static:{
             directory: path.resolve(__dirname, 'dist')
@@ -32,7 +36,22 @@ module.exports = {
                     'css-loader',
                     'sass-loader'
                 ]
+            },
+            {
+                test:/\.js$/,
+                exclude:/node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource"
             }
+            
         ]
     },
     plugins: [
@@ -40,6 +59,7 @@ module.exports = {
             title: 'webpack App',
             filename: 'index.html',
             template: 'src/template.html',
-        })
+        }),
+        new BundleAnalyzerPlugin(),
     ]
 }
